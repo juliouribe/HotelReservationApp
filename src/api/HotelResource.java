f0f4a6663b7aa9e54dd42d4695eea80bb88b2498
Julio C. Uribe <uribejulioc@gmail.com>
2022-12-23 18:51:47 -0800

    Create HotelResource API class

--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -2,8 +2,13 @@ package api;
 
 import Models.Customer;
 import Models.IRoom;
+import Models.Reservation;
 import service.CustomerService;
 import service.ReservationService;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.NoSuchElementException;
 
 public class HotelResource {
 
@@ -15,5 +20,21 @@ public class HotelResource {
     }
     public IRoom getRoom(String roomNumber) {
         return ReservationService.getInstance().getARoom(roomNumber);
+    }
+    public Reservation bookARoom (String customerEmail, IRoom room, Date checkInDate, Date checkOutDate) {
+        try {
+            Customer bookingCustomer = CustomerService.getInstance().getCustomer(customerEmail);
+            return ReservationService.getInstance().reserveARoom(bookingCustomer, room, checkInDate, checkOutDate);
+        } catch (NoSuchElementException ex) {
+            System.out.println("No customer found with that email: " + customerEmail);
+        }
+        return null; //Better way?
+    }
+    public Collection<Reservation> getCustomerReservations(String customerEmail) {
+        Customer bookingCustomer = CustomerService.getInstance().getCustomer(customerEmail);
+        return ReservationService.getInstance().getCustomerReservation(bookingCustomer);
+    }
+    public Collection<IRoom> findARoom(Date checkIn, Date checkOut) {
+        return ReservationService.getInstance().findRooms(checkIn, checkOut);
     }
 }
